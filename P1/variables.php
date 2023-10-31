60f85308c030663d62fbb63803d1dc73d8cfe862
soulaimanedahi <slimane.dahi@gmail.com>
2023-10-31 19:36:58 +0100

    change variables.php

--- P1/variables.php
+++ P1/variables.php
@@ -4,6 +4,12 @@ $isAllowedToEnter = "Oui";
 // SI on a l'autorisation d'entrer
 if ($isAllowedToEnter == "Oui") {
     // instructions à exécuter quand on est autorisé à entrer
+    
+    echo '<h1>Ma page web</h1>';
+    echo '<p>Aujourd\'hui nous sommes le ' . date('d/m/Y h:i:s') . '.</p>';
+    $fullname = 'Mathieu Nebra';
+    echo 'Bonjour ' . $fullname . ' et bienvenue sur le site !'; // OK
+
 } // SINON SI on n'a pas l'autorisation d'entrer
 elseif ($isAllowedToEnter == "Non") {
     // instructions à exécuter quand on n'est pas autorisé à entrer
