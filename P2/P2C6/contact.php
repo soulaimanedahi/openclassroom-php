b14d64d2bf82d2eb17c9c78585cdd81455b0c511
soulaimanedahi <slimane.dahi@gmail.com>
2023-11-08 00:04:35 +0100

    inputs validation

--- P2/P2C6/contact.php
+++ P2/P2C6/contact.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de Recettes - Page d'accueil</title>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-        rel="stylesheet"
-    >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
 
-    <?php include_once('header.php'); ?>
+        <?php include_once('header.php'); ?>
+
         <h1>Contactez nous</h1>
         <form>
             <div class="mb-3">
@@ -32,4 +32,5 @@
 
     <?php include_once('footer.php'); ?>
 </body>
+
 </html>
