11da75f8a9f4c3bed38a85ac3aafb0b1489e304b
soulaimanedahi <slimane.dahi@gmail.com>
2023-11-08 23:46:17 +0100

    login.php

--- P3/P3C2/home.php
+++ P3/P3C2/home.php
@@ -1,31 +1,31 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de Recettes - Page d'accueil</title>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-        rel="stylesheet"
-    >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
 
-    <?php include_once('header.php'); ?>
+        <?php include_once('header.php'); ?>
         <h1>Site de Recettes !</h1>
 
         <!-- Plus facile à lire -->
-        <?php foreach(get_recipes($recipes, $limit) as $recipe) : ?>
+        <?php foreach (get_recipes($recipes, $limit) as $recipe) : ?>
             <article>
-                <h3><?php echo($recipe['title']); ?></h3>
-                <div><?php echo($recipe['recipe']); ?></div>
-                <i><?php echo(display_author($recipe['author'], $users)); ?></i>
+                <h3><?php echo ($recipe['title']); ?></h3>
+                <div><?php echo ($recipe['recipe']); ?></div>
+                <i><?php echo (display_author($recipe['author'], $users)); ?></i>
             </article>
         <?php endforeach ?>
     </div>
 
     <?php include_once('footer.php'); ?>
 </body>
+
 </html>
