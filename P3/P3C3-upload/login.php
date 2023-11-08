e043b3b24f22c3358d3272cf43bf6085bda66ac0
soulaimanedahi <slimane.dahi@gmail.com>
2023-11-08 15:43:54 +0100

    form file input

--- P3/P3C3-upload/login.php
+++ P3/P3C3-upload/login.php
@@ -16,7 +16,8 @@ if (isset($postData['email']) &&  isset($postData['password'])) {
              * @TODO Formateur : le cours doit expliquer la fonction sprintf
              * (+ lien vers la doc FR)
              */
-            $errorMessage = sprintf('Les informations envoyées ne permettent pas de vous identifier : (%s/%s)',
+            $errorMessage = sprintf(
+                'Les informations envoyées ne permettent pas de vous identifier : (%s/%s)',
                 $postData['email'],
                 $postData['password']
             );
@@ -25,26 +26,26 @@ if (isset($postData['email']) &&  isset($postData['password'])) {
 }
 ?>
 
-<?php if(!isset($loggedUser)): ?>
-<form action="home.php" method="post">
-    <?php if(isset($errorMessage)) : ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo($errorMessage); ?>
+<?php if (!isset($loggedUser)) : ?>
+    <form action="home.php" method="post">
+        <?php if (isset($errorMessage)) : ?>
+            <div class="alert alert-danger" role="alert">
+                <?php echo ($errorMessage); ?>
+            </div>
+        <?php endif; ?>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" placeholder="you@exemple.com">
+            <div id="email-help" class="form-text">L'email utilisé lors de la création de compte.</div>
         </div>
-    <?php endif; ?>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" placeholder="you@exemple.com">
-        <div id="email-help" class="form-text">L'email utilisé lors de la création de compte.</div>
-    </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Mot de passe</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
-    <button type="submit" class="btn btn-primary">Envoyer</button>
-</form>
-<?php else: ?>
+        <div class="mb-3">
+            <label for="password" class="form-label">Mot de passe</label>
+            <input type="password" class="form-control" id="password" name="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>
+<?php else : ?>
     <div class="alert alert-success" role="alert">
-        Bonjour <?php echo($loggedUser['email']); ?> !
+        Bonjour <?php echo ($loggedUser['email']); ?> !
     </div>
 <?php endif; ?>
--- P3/P3C3-upload/submit_contact.php
+++ P3/P3C3-upload/submit_contact.php
@@ -2,40 +2,62 @@
 
 $postData = $_POST;
 
-if (!isset($postData['email']) || !isset($postData['message']))
-{
-	echo('Il faut un email et un message pour soumettre le formulaire.');
+if (!isset($postData['email']) || !isset($postData['message'])) {
+    echo ('Il faut un email et un message pour soumettre le formulaire.');
     return;
-}	
+}
 
 $email = $postData['email'];
 $message = $postData['message'];
+
+$success = false;
+
+// Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0) {
+    // Testons si le fichier n'est pas trop gros
+    if ($_FILES['screenshot']['size'] <= 1000000) {
+        // Testons si l'extension est autorisée
+        $fileInfo = pathinfo($_FILES['screenshot']['name']);
+        $extension = $fileInfo['extension'];
+        $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+        if (in_array($extension, $allowedExtensions)) {
+            // On peut valider le fichier et le stocker définitivement
+            $success = move_uploaded_file($_FILES['screenshot']['tmp_name'], 'uploads/' .
+                basename($_FILES['screenshot']['name']));
+            // echo "L'envoi a bien été effectué !";
+        }
+    }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site de Recettes - Contact reçu</title>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-        rel="stylesheet"
-    >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
     <div class="container">
 
-    <?php include_once('header.php'); ?>
+        <?php include_once('header.php'); ?>
         <h1>Message bien reçu !</h1>
-        
+
         <div class="card">
-            
+
             <div class="card-body">
                 <h5 class="card-title">Rappel de vos informations</h5>
-                <p class="card-text"><b>Email</b> : <?php echo($email); ?></p>
+                <p class="card-text"><b>Email</b> : <?php echo ($email); ?></p>
                 <p class="card-text"><b>Message</b> : <?php echo strip_tags($message); ?></p>
             </div>
+            <?php if ($success) : ?>
+                <div class="alert alert-success col-sm-4 m-2" role="alert">
+                    <?php echo ("L'envoi du fichier a bien été effectué !"); ?>
+                </div>
+            <?php endif; ?>
         </div>
